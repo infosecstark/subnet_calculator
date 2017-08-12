71b1f64cbd160a38a9959d693e25ac13671d1ce3
infosecstark <campagnanomattia@gmail.com>
2017-08-11 22:03:57 -0400

    Update subnet_calculator.bash

--- subnet_calculator.bash
+++ subnet_calculator.bash
@@ -2,19 +2,25 @@
 echo -n "Enter IP address: "
 read ip
 octet1=$(echo $ip | cut -d "." -f1) 
-#echo "${octet1}"
+
+#stores the first octet in a variable in order to define the default IP address class.
+#echo "${octet1}" #optional echo statement for debugging purposes
+
 if [[ $octet -le 127 ]]
 then
-    string=1
-    #echo "string is $string"     
+    string=1  #class A IP address
+    #echo "string is $string" #optional echo statement for debugging purposes only     
+    
 elif [[ $octet1 -le 128 ]] && [[ $octet -le 191 ]]
 then
-     string=2
-     #echo "string is $string"
+     string=2 #class B IP address
+     #echo "string is $string" 
+     
 elif [[ $octet1 -ge 192 ]] && [[ octet1 -le 223 ]]
 then 
-     string =3
+     string =3 #class C IP address
      #echo "string is $string"
+     
 else:
     echo "invalid subnet mask"
 fi
@@ -22,9 +28,14 @@ fi
 echo -n "Enter CIDR: "
 read cidr
 echo -n "Enter total bits for IPv4 address: "
+#enter the value32 here (total bits in an IPv4 IP address)
+
 read len
 
 borrowed_bits="$((len-cidr))"
+
+# variable storing the bits borrowed from the host portion
+
 echo "The CIDR entered borrows ${borrowed_bits} bits"
 case "$string" in
     "1") echo "Class A IP address" ;;
@@ -36,5 +47,5 @@ esac
 correction=2
 networks=$((2**borrowed_bits))
 hosts="$((networks-correction))"
-echo "This subnet mask allows to create ${networks} networks and ${hosts} hosts"
+echo "The supplied subnet mask allows to create ${networks} networks and ${hosts} hosts"
  
