a23648b70fa1b4e33586ac3094788a8973a92ce5
infosecstark <campagnanomattia@gmail.com>
2017-08-11 22:41:03 -0400

    Update subnet_calculator.bash

--- subnet_calculator.bash
+++ subnet_calculator.bash
@@ -1,30 +1,7 @@
 #!/bin/bash
 echo -n "Enter IP address: "
 read ip
-octet1=$(echo $ip | cut -d "." -f1) 
-
-#stores the first octet in a variable in order to define the default IP address class.
-#echo "${octet1}" #optional echo statement for debugging purposes
-
-if [[ $octet -le 127 ]]
-then
-    string=1  #class A IP address
-    #echo "string is $string" #optional echo statement for debugging purposes only     
-    
-elif [[ $octet1 -le 128 ]] && [[ $octet -le 191 ]]
-then
-     string=2 #class B IP address
-     #echo "string is $string" 
-     
-elif [[ $octet1 -ge 192 ]] && [[ octet1 -le 223 ]]
-then 
-     string =3 #class C IP address
-     #echo "string is $string"
-     
-else:
-    echo "invalid subnet mask"
-fi
-    
+   
 echo -n "Enter CIDR: "
 read cidr
 echo -n "Enter total bits for IPv4 address: "
@@ -37,12 +14,6 @@ borrowed_bits="$((len-cidr))"
 # variable storing the bits borrowed from the host portion
 
 echo "The CIDR entered borrows ${borrowed_bits} bits from the host portion"
-case "$string" in
-    "1") echo "Class A IP address" ;;
-     "2") echo "Class B IP address" ;;
-     "3") echo "Class C IP address" ;;
-                                 
-esac
 
 correction=2
 networks=$((2**borrowed_bits))
